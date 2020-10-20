8bc482fa8e34d1aa588303b2c7c0d7b3956990fd
Timothy7310 <penlist127@gmail.com>
2020-10-20 21:00:51 +0300

    create: added favicon and button back-to-top

--- js/main.js
+++ js/main.js
@@ -205,5 +205,22 @@ $(".think-about-form").each(function () {
   });
 
 
+  function backToTop(){
+    let button = $('.back-to-top');
+
+    $(window).on('scroll', () => {
+      if($(this).scrollTop() >= 50){
+        button.fadeIn();
+      }else {
+        button.fadeOut();
+      }
+    });
+    button.on('click', (e) => {
+      e.preventDefault();
+      $('html').animate({scrollTop: 0}, 1000);
+    })
+  }
+  backToTop();
+
 });
 
